ff950249e8181cefd7d5fc74fa4bc71ebff36183
Stein-Gate <1025118760@qq.com>
2018-09-28 17:11:36 +0800

    Spring 初步完成
    Spring 初步完成

--- src/main/Java/com/market/util/RedisUtil.java
+++ src/main/Java/com/market/util/RedisUtil.java
@@ -1,6 +1,67 @@
 package com.market.util;
 
-public class RedisUtil
-{
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
+import java.lang.reflect.Method;
+
+/**
+ *  通过Spring  管理redis 缓存配置
+ * @author Cherry
+ */
+
+@Configuration
+@EnableCaching
+public class RedisUtil extends CachingConfigurerSupport{
+    private volatile JedisConnectionFactory jedisConnectionFactory;
+    private volatile RedisTemplate<String,String> redisTemplate;
+    private volatile RedisCacheManager redisCacheManager;
+
+    /**
+     * 无参数的构造方法
+     */
+    public RedisUtil(){
+        super();
+    }
+
+    public RedisUtil(JedisConnectionFactory jedisConnectionFactory,
+                     RedisTemplate<String,String> redisTemplate,
+                     RedisCacheManager redisCacheManager){
+        this.jedisConnectionFactory = jedisConnectionFactory;
+        this.redisTemplate = redisTemplate;
+        this.redisCacheManager = redisCacheManager;
+    }
+
+    public JedisConnectionFactory getJedisConnectionFactory() {
+        return jedisConnectionFactory;
+    }
+
+    public RedisTemplate<String, String> getRedisTemplate() {
+        return redisTemplate;
+    }
+
+    public RedisCacheManager getRedisCacheManager() {
+        return redisCacheManager;
+    }
+
+    public KeyGenerator customKeyGenerator(){
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object o, Method method, Object... objects) {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(o.getClass().getName());
+                stringBuilder.append(method.getName());
+                for (Object obj : objects){
+                    stringBuilder.append(obj.toString());
+                }
+                return stringBuilder.toString();
+            }
+        };
+    }
 }
